b07278849e039fc0cf4db5fe33c970fcb5bdd1a5
Chance Callahan <chance1callahan@gmail.com>
2018-12-12 14:34:59 -0500

    Made it less classy and more simple.

--- src/main.py
+++ src/main.py
@@ -39,170 +39,156 @@ url = "https://www.chancecallahan.com/mirata/snap/alephone.snap"
 act_os = 60
 sup_os = ['Ubuntu', 'Fedora', 'Arch', 'Gentoo', 'openSUSE', 'Sabayon']
 
-class mirata:
-
-    def __init__(self, cls):
-        mirata.get_args(self, cls)
-
-    def get_args(self, cls):
-        global refactor_override
-        global target_os
-        global snap_bugout
-        global live_dangerously
-        global nerf_snapd
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--override-snap-bugout", dest="snap_bugout", help="Overrides sanity check for snapcraft.", action="store_true", default=False)
-        parser.add_argument("--target-os", dest="target_os", help="Select target OS, see --eligible-targets", action="store")
-        parser.add_argument("--eligible-targets", help="Eligible Target Platforms: Ubuntu, Fedora, Arch, Gentoo, openSUSE, Sabayon")
-        parser.add_argument("--refactor-override", dest="refactor_override", help="No one in their sane mind would use this flag right now.", action="store_true", default=False)
-        parser.add_argument("--live-dangerously", dest="live_dangerously", help="Just used for debug purposes. It won't exist in the final release.", action="store_true", default=False)
-        parser.add_argument("--nerf-snapd", dest="nerf_snapd", help="Nerfs snapd detection for debug purposes.", action="store_true", default=False)
-        args = parser.parse_args()
-        print parser.parse_args()
-        refactor_override = args.refactor_override
-        live_dangerously = args.live_dangerously
-        snap_bugout = args.snap_bugout
-        target_os = args.target_os
-        nerf_snapd = args.nerf_snapd
+def get_args():
+    global refactor_override
+    global target_os
+    global snap_bugout
+    global live_dangerously
+    global nerf_snapd
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--override-snap-bugout", dest="snap_bugout", help="Overrides sanity check for snapcraft.", action="store_true", default=False)
+    parser.add_argument("--target-os", dest="target_os", help="Select target OS, see --eligible-targets", action="store")
+    parser.add_argument("--eligible-targets", help="Eligible Target Platforms: Ubuntu, Fedora, Arch, Gentoo, openSUSE, Sabayon")
+    parser.add_argument("--refactor-override", dest="refactor_override", help="No one in their sane mind would use this flag right now.", action="store_true", default=False)
+    parser.add_argument("--live-dangerously", dest="live_dangerously", help="Just used for debug purposes. It won't exist in the final release.", action="store_true", default=False)
+    parser.add_argument("--nerf-snapd", dest="nerf_snapd", help="Nerfs snapd detection for debug purposes.", action="store_true", default=False)
+    args = parser.parse_args()
+    print parser.parse_args()
+    refactor_override = args.refactor_override
+    live_dangerously = args.live_dangerously
+    snap_bugout = args.snap_bugout
+    target_os = args.target_os
+    nerf_snapd = args.nerf_snapd
     #    refactor_override = 
-        cls.refactor_safety(self, cls)
+    refactor_safety( )
 
     # This subroutine basically keeps some idiot from running the program in it's current state. Like us.
 
-    def refactor_safety(self, cls):
-        if refactor_override == True:
-            print("Bless your heart, you stupid fool. Running as normal, and may God have mercy on your computer.")
-            # At some point, we should add the call to the subrouting that runs this mess.
-            cls.os_preprocessing(self, cls)
-        else:
-            print(refactor_override)
-            print(nerf_snapd)
-            print("Just... don't even bother trying to run this code right now. You'll need to perform an exorcism if you do, and you probably can't afford both a technomancer and whatever the hourly rate right now the Catholic Church is charging.")
+def refactor_safety( ):
+    if refactor_override == True:
+        print("Bless your heart, you stupid fool. Running as normal, and may God have mercy on your computer.")
+        # At some point, we should add the call to the subrouting that runs this mess.
+        os_preprocessing( )
+    else:
+        print(refactor_override)
+        print(nerf_snapd)
+        print("Just... don't even bother trying to run this code right now. You'll need to perform an exorcism if you do, and you probably can't afford both a technomancer and whatever the hourly rate right now the Catholic Church is charging.")
 
-    def os_preprocessing(self, cls):
-        global act_os
-        if target_os == "Ubuntu":
-            act_os=0
-        elif target_os == "Fedora":
-            act_os=1
-        elif target_os == "Arch":
-            act_os=2
-        elif target_os == "Gentoo":
-            act_os=3
-        elif target_os == "openSUSE":
-            act_os=4
-        elif target_os == "Sabayon":
-            act_os=5
-        else:
-            cls.title_banner(self, cls)
+def os_preprocessing( ):
+    global act_os
+    if target_os == "Ubuntu":
+        act_os=0
+    elif target_os == "Fedora":
+        act_os=1
+    elif target_os == "Arch":
+        act_os=2
+    elif target_os == "Gentoo":
+        act_os=3
+    elif target_os == "openSUSE":
+        act_os=4
+    elif target_os == "Sabayon":
+        act_os=5
+    else:
+        title_banner( )
 
-    def title_banner(self, cls):
-        os.system('clear')
-        print("mirata, the Linux setup tool for AlephOne")
-        print("This program comes with ABSOLUTELY NO WARRANTY.")
-        print("This is free software, and you are welcome to redistribute it under certain conditions.")
-        print( '-' * 20 )
-        time.sleep(3)
-        cls.check_for_snap_nerf(self, cls)
+def title_banner( ):
+    os.system('clear')
+    print("mirata, the Linux setup tool for AlephOne")
+    print("This program comes with ABSOLUTELY NO WARRANTY.")
+    print("This is free software, and you are welcome to redistribute it under certain conditions.")
+    print( '-' * 20 )
+    time.sleep(3)
+    check_for_snap_nerf( )
 
-    def check_for_snap_nerf(self, cls):
-        q = os_install(os_install)
-        if nerf_snapd == True:
-            print("Acting as if snapd is not installed.")
-            q.os_routing(os_install)
-        else:
-            cls.check_for_snap
+def check_for_snap_nerf( ):
+    if nerf_snapd == True:
+        print("Acting as if snapd is not installed.")
+        os_routing
+    else:
+        check_for_snap
 
-    def check_for_snap(self, cls):
-        q = os_install(os_install)
-        # Checks if snap is in the system PATH
-        if which('snap') is not None:
-            cls.os_select(self, cls)
-        else:
-            print("Hmm. I don't see snapd installed. Let's get that fixed.")
-            q.os_routing(os_install)
+def check_for_snap():
+    # Checks if snap is in the system PATH
+    if which('snap') is not None:
+        os_select()
+    else:
+        print("Hmm. I don't see snapd installed. Let's get that fixed.")
+        os_routing()
 
-    def os_select(self, cls):
-        global act_os
-        if act_os == 60:
-            print("Before we begin, please select your distro")
-            print("------------------------------------------")
-            print("1)Fedora")
-            print("2)Ubuntu")
-            print("3)Arch")
-            print("4)Gentoo")
-            print("5)openSUSE")
-            print("6)Sabayon(Equo)")
-            print("")
-            act_os = input("Selection: ")
-            act_os = act_os - 1
-        else:
-            print("I see you have selected " + sup_os[act_os] + " as your install target during runtime. Continuing unattended.")
-            cls.snap_dl(self, cls)
+def os_select( ):
+    global act_os
+    if act_os == 60:
+        print("Before we begin, please select your distro")
+        print("------------------------------------------")
+        print("1)Fedora")
+        print("2)Ubuntu")
+        print("3)Arch")
+        print("4)Gentoo")
+        print("5)openSUSE")
+        print("6)Sabayon(Equo)")
+        print("")
+        act_os = input("Selection: ")
+        act_os = act_os - 1
+    else:
+        print("I see you have selected " + sup_os[act_os] + " as your install target during runtime. Continuing unattended.")
+        snap_dl( )
 
-    def snap_dl(self, cls):
-        print('Downloading the snap file...')
-        call(["wget", url])
-        cls.installmirata(self, cls)
+def snap_dl( ):
+    print('Downloading the snap file...')
+    call(["wget", url])
+    installmirata( )
 
 
-    def bugout_nosnap(self, cls):
-        #Checks if we are overriding the bugout
-        if snap_bugout is True:
-            cls.snap_dl(self, cls)
-        else:
-            print("I can't seem to find snap on this system. Either add it to your path, or use --override-snap-bugout to bypass this sanity check")
+def bugout_nosnap( ):
+    #Checks if we are overriding the bugout
+    if snap_bugout is True:
+        snap_dl( )
+    else:
+        print("I can't seem to find snap on this system. Either add it to your path, or use --override-snap-bugout to bypass this sanity check")
 
-    def installmirata(self, cls):
-        if live_dangerously == True:
-            print("Installing Aleph One using snap. Stand by.")
-            call(["snap", "install", "alephone.snap", "--dangerous", "--devmode"])
-        else:
-            print("Use --live-dangerously")
+def installmirata( ):
+    if live_dangerously == True:
+        print("Installing Aleph One using snap. Stand by.")
+        call(["snap", "install", "alephone.snap", "--dangerous", "--devmode"])
+    else:
+        print("Use --live-dangerously")
 
-class os_install:
+def os_routing( ):
+    global act_os
+    if act_os == 0:
+        ubu_install()
+ #   elif act_os == 1:
+ #       fed_install()
+ #   elif act_os == 2:
+ #       arch_install()
+ #   elif act_os == 3:
+ #       needs_more_gentoo()
+ #   elif act_os == 4:
+ #       suse_install()
+ #   elif act_os == 5:
+ #       saba_install()
+    else:
+        print("Failure in the OS Routing Subroutine.")
 
-    def __init__(self, cls):
-        cls.os_routing(self, cls)
-
-    def os_routing(self, cls):
-        global act_os
-        if act_os == 0:
-            cls.ubu_install(self, cls)
-        elif act_os == 1:
-            cls.fed_install(self, cls)
-        elif act_os == 2:
-            cls.arch_install(self, cls)
-        elif act_os == 3:
-            cls.needs_more_gentoo(self, cls)
-        elif act_os == 4:
-            cls.suse_install(self, cls)
-        elif act_os == 5:
-            cls.saba_install(self, cls)
-        else:
-            print("Failure in the OS Routing Subroutine.")
-
-    def ubu_install(self, cls):
-        if act_os == 0:
-            print("I'm going to install snapd from your distribution's package manager.")
-            print("There shouldn't be any issues with the automated installation, but if you")
-            print("want to be extra cautious/paranoid, just install it the old fashioned way.")
-            print("")
-            print("")
-            print("I'm going to wait ten seconds for you to make up your mind, then I'll start.")
-            print("Hit Ctrl+C to stop this script.")
-            time.sleep(10)
-            print("Hold your noses, here we go!")
-#            call(["sudo" "apt" "install" "snapd" "-y"])
-            print("snapd has been installed. Resuming installation.")
-        else:
-            print("Failure in the snapd installation for Ubuntu subroutine.")
+def ubu_install( ):
+    if act_os == 0:
+        print("I'm going to install snapd from your distribution's package manager.")
+        print("There shouldn't be any issues with the automated installation, but if you")
+        print("want to be extra cautious/paranoid, just install it the old fashioned way.")
+        print("")
+        print("")
+        print("I'm going to wait ten seconds for you to make up your mind, then I'll start.")
+        print("Hit Ctrl+C to stop this script.")
+        time.sleep(10)
+        print("Hold your noses, here we go!")
+        call(["sudo" "apt" "install" "snapd" "-y"])
+        print("snapd has been installed. Resuming installation.")
+        snap_dl()
+    else:
+        print("Failure in the snapd installation for Ubuntu subroutine.")
 
 def main():
-    moo = mirata
-    moo(mirata)
-
-
+    get_args()
 
 if __name__ == '__main__':
     main()
